499dd94e74bc83739de6e7fbed2ffaae7b806bdc
benjaminlopez <benja248myn@gmail.com>
2024-12-14 19:28:56 -0300

    Mueve /src e index.html

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: "./public", // Indica a Vite dónde buscar el index.html
+  root: "", // Indica a Vite dónde buscar el index.html
   build: {
     outDir: "../dist", // Define la salida relativa al nuevo root
     emptyOutDir: true, // Limpia el directorio de salida antes de construir
